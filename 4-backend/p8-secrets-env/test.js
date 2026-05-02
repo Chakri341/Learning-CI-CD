38d7278809f00000fc76d01f43b5e5a69e53749c
Chakradhar <potluruchakradhar7@gmail.com>
2026-05-02 22:48:46 +0530

    issue in p8

--- 4-backend/p8-secrets-env/test.js
+++ 4-backend/p8-secrets-env/test.js
@@ -1,12 +1,17 @@
-const http = require("http");
-const app = require("./server");
+http.get("http://localhost:3001", (res) => {
+  let data = "";
 
-const server = http.createServer(app);
+  res.on("data", (chunk) => {
+    data += chunk;
+  });
 
-server.listen(3001, () => {
-  http.get("http://localhost:3001", (res) => {
+  res.on("end", () => {
     if (res.statusCode !== 200) {
-      throw new Error("Test failed ❌");
+      throw new Error("Status failed ❌");
+    }
+
+    if (!data || data === "undefined") {
+      throw new Error("Env variable missing ❌");
     }
 
     console.log("Test passed ✅");
